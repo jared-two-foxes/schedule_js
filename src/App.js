7280cf8a6eb47fc9eb36d41ef5e35cfafd0b6595
Jared Watt <Jared.Watt@eroad.com>
2019-12-18 10:04:54 +1300

    Pass authentication information to client side and hide sign in information when authenticated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import queryString from "query-string";
-
-import OpportunitiesHOC from "./OpportunitiesHOC";
 import ServicesHOC from "./ServicesHOC";
 
 import './App.scss';
 
 class App extends Component {
+  
   constructor(props) {
     super(props);
-    // this.state = {
-    //   authorized: false
-    // }
+    this.state = {
+      user: {},
+      error: null,
+      authenticated: false
+    };
   }
 
   componentDidMount() {
-      // var query = queryString.parse(window.location.search);
-      // if (query.token) {
-      //   this.setState({ ...this.state, authorized: true });
-      // }
+    fetch('http://localhost:3000/auth/login/success', {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true
+      }
+    })
+      .then(response => {
+        if (response.status == 200) return response.json();
+        throw new Error("failed to authenticate user");
+      })
+      .then(responseJson => {
+        this.setState({
+          authenticated: true, 
+          user: responseJson.user
+        })
+      })
+      .catch(error => {
+        this.setState({
+          authenticated: false,
+          error: "Failed to authenticate user"
+        })
+      });
   }
 
+  // updateOpportunity() {
+  //   console.log('updating the task!');
+  //   fetch('http://localhost:3000/updateTask', { method: 'PUT' })
+  //     .then(response => { 
+  //       console.log(response); 
+  //     })
+  //     .catch( e => {          
+  //       console.log(e);
+  //     });
+  // }
+
   render() {
+    const { authenticated } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a className="App-link" href="http://localhost:3000/auth/current" >
-            Log In
-          </a>
+          { !authenticated && <a className="App-link" href="http://localhost:3000/auth/current" > Log In </a> }
         </header>
-        {/* { this.state.authorized && <OpportunitiesHOC /> } */}
-        {/* <div><OpportunitiesHOC /></div> */}
+        {/* <div>
+          <button className="link" onClick={this.updateOpportunity}>
+              Update
+          </button>
+        </div> */}
         <div><ServicesHOC /></div>
       </div>
     );
